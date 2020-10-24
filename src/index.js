b713ef06ce5def673358b2f12fe3b580a257185e
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2020-10-25 02:26:04 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let word = [];
+    
+    for ( let i = 0 ; i <= expr.length - 9 ; i += 10 ) {
+        arr.push( expr.slice( i, i + 10 ) );
+    }
+
+    for ( let str of arr ) {
+
+        if ( str == '**********' ) {
+            word.push( ' ' );
+        } else {
+
+            let letter = [];
+            let morseLetter = [];
+
+            for ( let j = 0 ; j <= str.length ; j += 2 ) {
+                letter.push( str.slice( j, j + 2 ) );
+            }
+
+            for ( let morse of letter ) {
+                if ( morse == '10') morseLetter.push( '.' );
+                if ( morse == '11') morseLetter.push( '-' );
+            }
+            
+            morseLetter = morseLetter.join( '' );
+            word.push( MORSE_TABLE[morseLetter] );
+        }
+
+    }
+
+    return word.join( '' );
 }
 
 module.exports = {
